40f466f2c62546bdee815c2119d7ad2f4346d63e
aitageo <aitageo1980@hotmail.com>
2026-08-30 13:15:15 -0500

    incluido mapa

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
     window.addEventListener("scroll", function () {
         const header = document.querySelector("header");
+        const botonReserva = document.querySelector(".reservas");
 
         if (window.scrollY > 50) {
             header.classList.add("scrolled");
+            botonReserva.style.display = "block";
         } else {
             header.classList.remove("scrolled");
+            botonReserva.style.display = "none";
         }
     });
